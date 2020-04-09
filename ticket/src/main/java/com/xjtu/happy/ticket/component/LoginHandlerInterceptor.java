4ad749f81baa7ca83cd399c50ea3675712a2cc59
NoahPlus <ljg247552025@qq.com>
2020-04-09 20:28:18 +0800

    登录功能1.0

---
+++ ticket/src/main/java/com/xjtu/happy/ticket/component/LoginHandlerInterceptor.java
@@ -0,0 +1,40 @@
+package com.xjtu.happy.ticket.component;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@Component
+public class LoginHandlerInterceptor implements HandlerInterceptor{
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        Object user = request.getSession().getAttribute("loginUser");
+        if(user==null){
+            //未登录，返回登录界面
+            request.setAttribute("msg","没有权限，请先登录");
+            request.getRequestDispatcher("/index.html").forward(request,response);
+            return false;
+        }
+        else {
+            //已登录，放行
+            return true;
+        }
+
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        Object user = request.getSession().getAttribute("userName");
+        System.out.println("postCompletion----"+ user +" ::: "+ request.getRequestURL());
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        Object user = request.getSession().getAttribute("userName");
+        System.out.println("afterCompletion----"+ user +" ::: "+ request.getRequestURL());
+    }
+}
