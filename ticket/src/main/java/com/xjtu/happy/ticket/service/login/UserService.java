4ae2d42741b5577d01e534afee72ee3756bd8c2b
cangsaka <59947039+Better116@users.noreply.github.com>
2020-04-10 20:52:12 +0800

    Create UserService.java
    订票页面查询数据库中是否有该用户信息

---
+++ ticket/src/main/java/com/xjtu/happy/ticket/service/login/UserService.java
@@ -0,0 +1,18 @@
+package com.xjtu.happy.ticket.service;
+
+import com.xjtu.happy.ticket.bean.User;
+import com.xjtu.happy.ticket.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+
+@Service
+public class UserService {
+    @Autowired
+    UserMapper userMapper;
+    
+    public User FindUserByIdentityNum(String identityNum){
+    	return userMapper.FindUserByIdentityNum(identityNum);
+    }
+}
