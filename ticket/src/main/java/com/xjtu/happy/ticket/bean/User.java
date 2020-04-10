61c43460b2cd40b457b52a50bc5c055f24b3dd14
gzy_lio <492256684@qq.com>
2020-04-10 11:22:00 +0800

    修复bug

--- ticket/src/main/java/com/xjtu/happy/ticket/bean/User.java
+++ ticket/src/main/java/com/xjtu/happy/ticket/bean/User.java
@@ -6,6 +6,83 @@ import lombok.Setter;
 @Getter
 @Setter
 public class User {
-    private String id;
+    //用户id
+    private int userId;
+
+    //用户名
+    private String userName;
+
+    //密码
+    private String password;
+
+    //姓名
     private String name;
+
+    //证件号
+    private String identityNum;
+
+    //手机号
+    private String phone;
+
+    //邮箱
+    private String email;
+
+    //用户类型
+    private String type;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getIdentityNum() {
+        return identityNum;
+    }
+
+    public void setIdentityNum(String identityNum) {
+        this.identityNum = identityNum;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
