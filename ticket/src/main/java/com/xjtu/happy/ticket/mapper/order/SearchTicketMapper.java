cca389e5ea2a7c059e634b64280d4b06eb89e3da
cangsaka <59947039+Better116@users.noreply.github.com>
2020-04-10 20:56:55 +0800

    加上查询座位价格

--- ticket/src/main/java/com/xjtu/happy/ticket/mapper/order/SearchTicketMapper.java
+++ ticket/src/main/java/com/xjtu/happy/ticket/mapper/order/SearchTicketMapper.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @Mapper
 public interface SearchTicketMapper {
-    @Select("select DISTINCT t.trainId,t.trainNum,t.startTime,t.startStationName,t.endTime,ticketseat.travelTime as travelTime,t.endStationName,COALESCE(sa.A,0) as A,\n" +
+    @Select("select distinct t.trainId,t.trainNum,t.startTime,t.startStationName,t.endTime,ticketseat.travelTime as travelTime,t.endStationName,COALESCE(sa.A,0) as A,\n" +
             "COALESCE(sb.B,0) as B,COALESCE(sc.C,0) as C from ticketseat,train t \n" +
             "  LEFT JOIN\n" +
             "   (Select trainId,count(seatId) as A  from ticketseat \n" +
@@ -25,8 +25,8 @@ public interface SearchTicketMapper {
             "    where seatType = 'C' and ticketSeatStatus = 'NORMAL' and travelTime = #{date} GROUP BY trainId) sc ON t.trainId = sc.trainId\n" +
             "where t.startStationName=#{start} and t.endStationName=#{end} AND t.trainId=ticketseat.trainId AND ticketseat.travelTime=#{date}")
     public List<TicketLeft> queryTickets(String start, String end, Date date);
-    @Select("select t.trainId,t.trainNum,t.startTime,t.startStationName,t.endTime,t.endStationName,COALESCE(sa.ARemain,0) as ARemain,\n" +
-            "COALESCE(sb.BRemain,0) as BRemain,COALESCE(sc.CRemain,0) as CRemain from train t \n" +
+    @Select("select t.trainId,t.trainNum,t.startTime,t.startStationName,t.endTime,t.endStationName,COALESCE(sa.ARemain,0) as A,\n" +
+            "COALESCE(sb.BRemain,0) as B,COALESCE(sc.CRemain,0) as C,p.APrice,p.BPrice,p.CPrice from train t \n" +
             "  LEFT JOIN\n" +
             "   (Select trainId,count(seatId) as ARemain from ticketseat \n" +
             "    where seatType = 'A' and ticketSeatStatus = 'NORMAL' and travelTime = #{date} GROUP BY trainId) sa ON t.trainId = sa.trainId\n" +
@@ -36,6 +36,7 @@ public interface SearchTicketMapper {
             "  LEFT JOIN\n" +
             "   (Select trainId,count(seatId) as CRemain from ticketseat \n" +
             "    where seatType = 'C' and ticketSeatStatus = 'NORMAL' and travelTime = #{date} GROUP BY trainId) sc ON t.trainId = sc.trainId\n" +
-            "where t.trainId=id")
+            "  LEFT JOIN price p ON t.startStationid=p.startStationid AND t.endStationid=p.endStationid AND t.trainTypeId=p.trainTypeId\n" +
+            "where t.trainId=#{id}")
     public TicketLeft odTicket(int id, Date date);
 }
