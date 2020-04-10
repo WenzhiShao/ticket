20a2018969f7f569090bbede9101f586af440c06
cangsaka <59947039+Better116@users.noreply.github.com>
2020-04-10 20:40:36 +0800

    Update SearchController.java
    调整传参

--- ticket/src/main/java/com/xjtu/happy/ticket/controller/order/SearchController.java
+++ ticket/src/main/java/com/xjtu/happy/ticket/controller/order/SearchController.java
@@ -1,26 +1,24 @@
-package com.xjtu.happy.ticket.controller.order;
-/*
- 查询车票
-* */
+package com.xjtu.happy.ticket.controller;
+
 import com.xjtu.happy.ticket.bean.TicketLeft;
-import com.xjtu.happy.ticket.service.order.SearchTicketService;
+import com.xjtu.happy.ticket.service.QueryTicketService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import java.sql.Time;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
-@Controller
-public class SearchController {
-    @Autowired
-    SearchTicketService query;
-    @RequestMapping("/index/list")
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
+@Controller
+public class QueryController {
+    @Autowired
+    QueryTicketService query;
+    @RequestMapping("/index/list")
     public String querytickets(Model model, String start , String end,String date) throws ParseException {
         SimpleDateFormat  format = new SimpleDateFormat("yyyy-MM-dd");
         Date da= format.parse(date);         //字符串转化成Date类型（这里是java.util.Date）
@@ -28,31 +26,27 @@ public class SearchController {
         List<TicketLeft> ticketLefts = query.queryTickets(start, end,d);
         if (ticketLefts.isEmpty()){
             model.addAttribute("msg","暂无列车信息");
-            return "search";
+            return "query";
         }else {
-            System.out.println(ticketLefts);
-            model.addAttribute("tickets",ticketLefts);
-            return "ticket";
-        }
+            System.out.println(ticketLefts); 
+        model.addAttribute("tickets",ticketLefts);
+        return "ticket";
     }
-    //转跳到订票页面
-    @RequestMapping(value ="/o",method = RequestMethod.GET)
-    public String turnToOrder(Model model, @RequestParam String trainNum,@RequestParam Time startTime,
-                              @RequestParam String startst,@RequestParam Time endtime,@RequestParam String endst,
-                              @RequestParam Integer A ,@RequestParam Integer B,@RequestParam Integer C,@RequestParam java.sql.Date time){
+    }
 
-        model.addAttribute("num",trainNum);
-        model.addAttribute("sT",startTime);
-        model.addAttribute("sst",startst);
-        model.addAttribute("eT",endtime);
-        model.addAttribute("est",endst);
-        model.addAttribute("A",A);
-        model.addAttribute("B",B);
-        model.addAttribute("C",C);
-        model.addAttribute("time",time);
+    //进入预订页面
+    @RequestMapping(value ="/o",method = RequestMethod.GET)
+    public String turnToOrder(HttpServletRequest req, @RequestParam Integer trainId,@RequestParam java.sql.Date time){
+    	HttpSession session=req.getSession();
+
+    	TicketLeft ticketSelected = query.odTicket(trainId,time);
+    	ticketSelected.setTravelTime(time);
+        session.setAttribute("ticketSelected", ticketSelected);
+    
         return "order";
 
     }
+ 
     //进入查票页面
     @RequestMapping("/search")
     public String search(){
