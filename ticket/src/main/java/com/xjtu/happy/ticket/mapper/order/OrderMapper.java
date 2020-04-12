26a855b4e5f418056dfa04df496dd1b281a407d7
cangsaka <59947039+Better116@users.noreply.github.com>
2020-04-12 13:24:40 +0800

    根据乘车日期动态分配座位

--- ticket/src/main/java/com/xjtu/happy/ticket/mapper/order/OrderMapper.java
+++ ticket/src/main/java/com/xjtu/happy/ticket/mapper/order/OrderMapper.java
@@ -1,5 +1,6 @@
 package com.xjtu.happy.ticket.mapper.order;
 
+import java.util.Date;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
@@ -18,8 +19,8 @@ public interface OrderMapper {
 	public void saveOrder(Orders order);
 	
 	//找到分配座位的id
-	@Select("SELECT * FROM ticketseat WHERE trainId=#{trainId} AND seatType=#{seatType} AND Date(travelTime)='2020-04-11' AND ticketSeatStatus='NORMAL' LIMIT 1 FOR UPDATE")
-	public TicketSeat selectSeatByLock(@Param("trainId") int trainId,@Param("seatType") String seatType);
+	@Select("SELECT * FROM ticketseat WHERE trainId=#{trainId} AND seatType=#{seatType} AND Date(travelTime)=#{time} AND ticketSeatStatus='NORMAL' LIMIT 1 FOR UPDATE")
+	public TicketSeat selectSeatByLock(@Param("trainId") int trainId,@Param("seatType") String seatType,@Param("time") Date time);
 	
 	//根据id更新座位状态
 	@Update("UPDATE ticketseat SET ticketSeatStatus='SELL' WHERE seatId=#{seatId}")
