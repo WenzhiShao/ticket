cf601e1b1e0585be2427e9a1638e023f6509aba7
zhaoyu <1070717172@qq.com>
2020-04-17 22:11:34 +0800

    新增查询订单功能

---
+++ ticket/src/main/java/com/xjtu/happy/ticket/controller/order/SearchOrdersController.java
@@ -0,0 +1,30 @@
+package com.xjtu.happy.ticket.controller.order;
+
+import com.xjtu.happy.ticket.bean.SearchOrders;
+import com.xjtu.happy.ticket.service.order.SearchOrdersServe;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
+/*查询订单
+* */
+@Controller
+public class SearchOrdersController {
+    @Autowired
+    SearchOrdersServe Sorder;
+    @RequestMapping("/orders")
+    public String searchorders(HttpServletRequest request, Model model){
+        HttpSession session=request.getSession();
+        String username= (String) session.getAttribute("loginUser");
+        List<SearchOrders> serchorder = Sorder.serchorder(username);
+        model.addAttribute("orderlist",serchorder);
+        return "orders";
+    }
+}
---
+++ ticket/src/main/java/com/xjtu/happy/ticket/service/order/SearchOrdersServe.java
@@ -0,0 +1,18 @@
+package com.xjtu.happy.ticket.service.order;
+
+import com.xjtu.happy.ticket.bean.SearchOrders;
+import com.xjtu.happy.ticket.mapper.order.SearchOrdersMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+/*查询订单
+* */
+@Service
+public class SearchOrdersServe {
+    @Autowired
+    SearchOrdersMapper sercho;
+    public List<SearchOrders> serchorder(String username){
+        return sercho.searchorder(username);
+    }
+}
